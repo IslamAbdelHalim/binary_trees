b113d44aac8a03d0c0db5d5ef18fe01140dcfab2
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-01-31 02:46:50 +0200

    delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -13,15 +13,9 @@ void binary_tree_delete(binary_tree_t *tree)
 	}
 	else
 	{
-		if (tree->left != NULL)
-		{
-			binary_tree_delete(tree->left);
-		}
-		else if (tree->right != NULL)
-		{
-			binary_tree_delete(tree->right);
-		}
+		binary_tree_delete(tree->left);
 
+		binary_tree_delete(tree->right);
 		free(tree);
 	}
 }
