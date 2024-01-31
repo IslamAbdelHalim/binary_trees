a359ecba15a60b3fa135431e50b2dc73316d4fad
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-01-31 03:28:43 +0200

    betty

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,7 +6,7 @@
  *
  * @tree: pointer to the root of tree
  *
- * @func - is a pointer to a function to call for each node
+ * @func: is a pointer to a function to call for each node
 */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
