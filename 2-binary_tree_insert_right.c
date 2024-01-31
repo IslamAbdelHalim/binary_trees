2560c3110021ddedc8a885ac11ca777cf1343230
wessam18 <wessamabdelnabi903@gmail.com>
2024-01-31 02:25:53 +0200

    task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - fun to insert new node to the left
+ * @parent: the root of the tree
+ * @value: value of the node
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+binary_tree_t *newnode;
+
+    if (!parent)
+        return (NULL);
+
+    newnode = binary_tree_node(parent, value);
+    if (newnode == NULL)
+        return (NULL);
+
+    if (parent->right != NULL)
+    {
+        parent->right->parent = newnode;
+        newnode->right = parent->right;
+    }
+    parent->right = newnode;
+
+    return (newnode);
+}
