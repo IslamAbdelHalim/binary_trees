8c429b8eeeb0a1f4fe04c5cfb81cf13697b60f0c
wessam18 <wessamabdelnabi903@gmail.com>
2024-01-31 22:56:16 +0200

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,11 +20,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 	{
 		if (tree->left)
-			height_left += binary_tree_height(tree->left) + 1;
+			height_left += binary_tree_height(tree->left);
 		if (tree->right)
-			height_right += binary_tree_height(tree->right) + 1;
+			height_right += binary_tree_height(tree->right);
 
-		return ((height_left > height_right) ? height_left : height_right);
+		return (((height_left > height_right) ? height_left : height_right) + 1);
 	}
 }
 
