fd342a86726c64ecb42c75cf096711cf1bb598b5
wessam18 <wessamabdelnabi903@gmail.com>
2024-02-02 00:52:29 +0200

    task 24

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,28 +1,33 @@
 #include "binary_trees.h"
 #include <limits.h>
 /**
- * 
+ * helper_bst - fun to check binary scearch tree or not
+ * @tree: the tree to check
+ * @min: min value of current sub-tree
+ * @max: max value of current sub-tree
+ * Return: 1 if bst, 0 otherwise
 */
-int helper_bst(const binary_tree_t *tree)
+int helper_bst(const binary_tree_t *tree, int min, int max)
 {
-    if (!tree)
-        return (1);
+	if (!tree)
+		return (1);
 
-    if ((tree->left && tree->n > tree->left->n) || (tree->right && tree->n <= tree->right->n))
-        return (1);
-    else
-        return (0);
+	if (tree->n <= min || tree->n >= max)
+		return (0);
 
-    return (helper_bst(tree->left) &&
-        helper_bst(tree->right));
+	return (helper_bst(tree->left, min, tree->n) &&
+		helper_bst(tree->right, tree->n, max));
+
 }
 
 /**
- * 
+ * binary_tree_is_bst - fun to check bst or not
+ * @tree: tree to check it
+ * Return: as need
 */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    return (helper_bst(tree));
+	if (!tree)
+		return (0);
+	return (helper_bst(tree, INT_MIN, INT_MAX));
 }
