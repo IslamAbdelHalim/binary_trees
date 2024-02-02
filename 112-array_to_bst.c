7acc1afab17b6cf74ff5973665c0c797855d153b
wessam18 <wessamabdelnabi903@gmail.com>
2024-02-02 04:20:25 +0200

    task 26

---
+++ 112-array_to_bst.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * array_to_bst - fun to insert an array in bst
+ * @array: the arrey to insert
+ * @size: the size of the array
+ * Return: as need
+*/
+bst_t *array_to_bst(int *array, size_t size)
+{
+size_t i;
+bst_t *rroot;
+
+	if (!array || size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		bst_insert(&rroot, array[i]);
+	}
+	return (rroot);
+}
